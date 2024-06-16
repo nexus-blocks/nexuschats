1acc0597b7e662cd6bb110c859676014dbbd7178
iamenochchirima <iamenochchirima@gmail.com>
2024-06-16 12:53:45 +0200

    updates

--- src/nexuschats_backend/canisters/users/src/lib.rs
+++ src/nexuschats_backend/canisters/users/src/lib.rs
@@ -1,6 +1,5 @@
 #[macro_use]
 extern crate serde;
-use candid::{Decode, Encode};
 use ic_cdk::api::time;
 use ic_stable_structures::memory_manager::{MemoryId, MemoryManager, VirtualMemory};
 use ic_stable_structures::storable::Bound;
